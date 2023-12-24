34018cb4bd2c36669db1369a320d4ba1645a6df8
dsub <152191586+dsubdsub@users.noreply.github.com>
2023-12-24 08:21:17 -0300

    Add files via upload

---
+++ lec_1_area_of_a_circle.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Program name: Circle area calculation
+Author: dsubdsub
+Creation date: Mon Dec 11 16:47:50 2023
+Description: Lecture 1 - Program for calculating the area of a circle
+Repository: MIT_6.0001_Python
+Version: 1.0
+License: MIT
+"""
+import math
+
+#Loop to restart the program if the user wants to
+restart=0
+while restart==0:
+    print("\nPROGRAM TO CALCULATE THE AREA OF A CIRCLE\n")
+    while True:
+        try:
+            radius = float(input("Enter the radius of the circle: "))
+            print(f"You choose the radius {radius}. I'm glad, this is a valid input.")
+            break
+        except ValueError:
+            print("Try again and enter a valid raius, please.\n")
+        
+    # Area of circle equation 
+    circle_area = math.pi*(radius**2)
+    print(f"The area of a circle of radius {radius} is roughly {circle_area:.3f}.\n")
+    restart=int(input("0 - Restart\n1 - Quit\n"))
+    
+    
---
+++ lec_1_square_root_deduction.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Program name: Square root deduction
+Author: dsubdsub
+Creation date: Sun Dec 10 15:53:39 2023
+Description: Lecture 1 - Program for deducing square root of a number
+Repository: MIT_6.0001_Python
+Version: 1.0
+License: MIT
+"""
+
+import numpy as np
+
+#Loop to restart the program if the user wants to
+restart=0
+while restart==0:
+    print("\nPROGRAM TO DEDUCE THE SQUARE ROOT OF A NUMBER")
+    while True:
+        try:
+            x = float(input("\nEnter a number you want to get the square root of: "))
+            print(f"You choose the number {x}. I'm glad, this is a valid input.")
+            break
+        except ValueError:
+            print("Try again and enter a valid number, please.\n")
+        
+    g = 1
+    flag=0
+    while flag==0:
+        # 2 - If g*g is close enough to x, stop and say g is the answer 
+        if g*g >= x-0.0001 and g*g <= x + 0.0001:
+            flag=1
+            square_root = g
+            print(f"\nThe rough square root of {x} is {square_root:.5f}.\n")
+            restart=int(input("0 - Restart\n1 - Quit\n"))
+           
+        else:
+            g = np.mean([g,(x/g)])
